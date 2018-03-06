75b98f0aa2c626f41181ca2f4f511eb695225179
Takanashi <midlifeactor338@gmail.com>
2018-03-07 08:40:36 +0900

    Update to version 1.0.0

--- LineTimeline/__init__.py
+++ LineTimeline/__init__.py
@@ -1,10 +1,10 @@
 from .client import LineTimeline
 
 __copyright__       = 'Copyright 2018 by Takanashi'
-__version__         = '0.0.1'
+__version__         = '1.0.0'
 __license__         = 'BSD-3-Clause'
 __author__          = 'Takanashi'
 __author_email__    = 'tytyty311@cream.pink'
-__url__             = ''
+__url__             = 'https://github.com/sharplin/LineTimeline'
 
 __all__ = ['LineTimeline']
