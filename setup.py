4570377a0dd9e22349d114f596e4a089dc5c7898
Takanashi <midlifeactor338@gmail.com>
2018-03-07 00:07:43 +0900

    Update to version 1.0.0

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name = 'LineTimeline',
-    version = '0.0.1',
+    version = '1.0.0',
     description = 'Line TimeLine Api',
     author = 'Takanashi',
     author_email = 'tytyty311@cream.pink',
     install_requires = ['requests', 'json', 'lxml'],
-    url = '',
+    url = 'https://github.com/sharplin/LineTimeline',
     license = 'BSD-3-Clause',
     packages = ["LineTimeline"]
 )
