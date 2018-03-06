f03ac26158303d8df491039d72c27cc85fb330d9
Takanashi <midlifeactor338@gmail.com>
2018-03-06 23:55:15 +0900

    Add example

--- example/autoComment.py
+++ example/autoComment.py
@@ -8,6 +8,6 @@ while True:
     feed = client.getFeed()
     for x in feed["result"]["feeds"]:
         if x["post"]["postInfo"]["liked"] == False:
-            client.likePost(x["post"]["postInfo"]["homeId"])
-            client.createComment(x["post"]["postInfo"]["homeId"], "--CommentText--")
+            client.likePost(x["post"]["postInfo"]["postId"])
+            client.createComment(x["post"]["postInfo"]["postId"], "--CommentText--")
     time.sleep(60)
--- example/autoLike.py
+++ example/autoLike.py
@@ -8,5 +8,5 @@ while True:
     feed = client.getFeed()
     for x in feed["result"]["feeds"]:
         if x["post"]["postInfo"]["liked"] == False:
-            client.likePost(x["post"]["postInfo"]["homeId"])
+            client.likePost(x["post"]["postInfo"]["postId"])
     time.sleep(60)
